57982a86d00c9853cf74a12fb42b643ea142ffeb
cody1991 <codytang@tencent.com>
2020-11-18 23:53:46 +0800

    feat: add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import Counter from './components/Counter';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* <HelloWorld name="codytang" /> */}
       {/* <Counter /> */}
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
