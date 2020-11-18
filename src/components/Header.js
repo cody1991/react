3ac646656c76d4000d7858558dec8bc5ff7d55f9
cody1991 <codytang@tencent.com>
2020-11-19 00:04:23 +0800

    feat: add nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import Navigation from './Navigation';
+
 export default function Header() {
-  return <header className="border-b font-bold p-3">Header</header>;
+  return (
+    <header className="border-b font-bold p-3">
+      Header
+      <Navigation></Navigation>
+    </header>
+  );
 }
