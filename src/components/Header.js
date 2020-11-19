f1658d607557c597ae54089b69f2feb24ec26b3c
cody1991 <codytang@tencent.com>
2020-11-19 23:14:17 +0800

    feat: modify nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import Navigation from './Navigation';
 
 export default function Header() {
   return (
-    <header className="border-b font-bold p-3">
-      Header
+    <header className="border-b p-3 flex justify-between items-center">
+      <span className="font-bold">Header</span>
       <Navigation></Navigation>
     </header>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,17 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export default function Navigation() {
-  return <nav className="">the nav</nav>;
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <nav>
+      <span className="text-xl">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => setShowMenu(!showMenu)}
+        ></FontAwesomeIcon>
+      </span>
+    </nav>
+  );
 }
