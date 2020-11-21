c3b0bb51e387da33213f3870b526a849e4a57d70
cody1991 <codytang@tencent.com>
2020-11-21 12:17:11 +0800

    feat: add nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,24 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
+  let menu;
+  let menuMask;
+
+  if (showMenu) {
+    menu = (
+      <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+        Menu
+      </div>
+    );
+
+    menuMask = (
+      <div
+        className="bg-black-t-50 fixed top-0 left-0 h-full w-full z-49"
+        onClick={() => setShowMenu(false)}
+      ></div>
+    );
+  }
+
   return (
     <nav>
       <span className="text-xl">
@@ -12,6 +30,9 @@ export default function Navigation() {
           onClick={() => setShowMenu(!showMenu)}
         ></FontAwesomeIcon>
       </span>
+
+      {menu}
+      {menuMask}
     </nav>
   );
 }
