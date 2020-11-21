490034734a2a3f461b7cec6c14dd70cccb7b90d8
cody1991 <codytang@tencent.com>
2020-11-21 19:23:49 +0800

    feat: add product detail view

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,22 +1,44 @@
 import Axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Loader from '../components/Loader';
 export default function About() {
   // https://mockapi.io/projects/5fb8b6472f145f0016c3c7be
   const { id } = useParams();
   const url = `https://5fb8b6472f145f0016c3c7bd.mockapi.io/products/${id}`;
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   // 通过使用这个Hook，你可以告诉React 组件需要在渲染后执行某些操作。
   useEffect(() => {
-    Axios.get(url).then((res) => setProduct(res.data));
+    setLoading(true);
+    setError(false);
+    Axios.get(url)
+      .then((res) => {
+        setProduct(res.data);
+        setLoading(true);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
   }, [url]);
 
   let content = <div></div>;
 
+  if (error) {
+    content = <span>There was an error please refresh or try again later</span>;
+  }
+
+  if (loading) {
+    content = <Loader></Loader>;
+  }
+
   if (product) {
     content = (
       <div>
+        <span>{loading}</span>
         <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
         <div>
           <img src={product.images} alt={product.name} />
